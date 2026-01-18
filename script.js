7950684e956bacc7561fc6d2abbb9a9f30ffd014
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-18 16:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let submitBtn = document.getElementById("submit");
+let bookList = document.getElementById("book-list");
+
+submitBtn.addEventListener("click", function () {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let isbn = document.getElementById("isbn").value;
+
+  if (title === "" || author === "" || isbn === "") return;
+
+  let row = document.createElement("tr");
+
+  row.innerHTML = `
+    <td>${title}</td>
+    <td>${author}</td>
+    <td>${isbn}</td>
+    <td><button class="delete">Clear</button></td>
+  `;
+
+  bookList.appendChild(row);
+
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+});
+
+bookList.addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.parentElement.remove();
+  }
+});
